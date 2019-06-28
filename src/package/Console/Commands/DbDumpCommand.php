d5fd4236bf3bac509257a410664d5cc3830cadae
Vladimir Ković <vlada.kovic@gmail.com>
2019-06-28 18:30:42 +0200

    db:dump better formatting

--- src/package/Console/Commands/DbDumpCommand.php
+++ src/package/Console/Commands/DbDumpCommand.php
@@ -4,12 +4,13 @@ namespace Vkovic\LaravelCommandos\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Vkovic\LaravelCommandos\Console\FormatOutput;
 use Vkovic\LaravelCommandos\Handlers\Console\WithConsoleHandler;
 use Vkovic\LaravelCommandos\Handlers\Database\WithDbHandler;
 
 class DbDumpCommand extends Command
 {
-    use WithConsoleHandler, WithDbHandler;
+    use WithConsoleHandler, WithDbHandler, FormatOutput;
 
     /**
      * The name and signature of the console command.
@@ -17,7 +18,8 @@ class DbDumpCommand extends Command
      * @var string
      */
     protected $signature = 'db:dump
-                                {database? : Db (name) to be dump. If omitted, it`ll use default db name from env}';
+                                {database? : Db (name) to be dump. If omitted, it`ll use default db name from env}
+                                {--dir= : Directory for dump creation. If omitted default filesystem dir will be used}';
 
     /**
      * The console command description.
@@ -32,22 +34,20 @@ class DbDumpCommand extends Command
         // Implement arguments that user can pass to customize dump process.
         // Also add easy gzip fn
 
-        // Get database name either from passed argument (if any)
-        // or from default database configuration
-        $database = $this->argument('database') ?: (function () {
-            $default = config('database.default');
+        $database = $this->argument('database')
+            ?: config('database.connections.' . config('database.default') . '.database');
 
-            return config("database.connections.$default.database");
-        })();
+        $dir = $this->option('dir')
+            ?: config('filesystems.disks.' . config('filesystems.default') . '.root');
 
         if (!$this->dbHandler()->databaseExists($database)) {
-            return $this->warn("Database `$database` doesn`t exist");
+            return $this->skipLine()->warn("Database `$database` doesn`t exist");
         }
 
         $user = env('DB_USERNAME');
         $password = env('DB_PASSWORD');
         $host = env('DB_HOST');
-        $destination = storage_path("$database-" . Carbon::now()->format('Y-m-d-H-i-s') . '.sql');
+        $destination = $dir . DIRECTORY_SEPARATOR . "$database-" . Carbon::now()->format('Y-m-d-H-i-s') . '.sql';
 
         // Omit view definer so we do not come across missing user on another system
         $removeDefiner = "| sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/'";
@@ -59,7 +59,7 @@ class DbDumpCommand extends Command
 
         $this->consoleHandler()->executeCommand($command);
 
-        $this->info("Database `$database` dumped");
+        $this->skipLine()->info("Database `$database` dumped");
         $this->line("Destination: `$destination`");
     }
 }
