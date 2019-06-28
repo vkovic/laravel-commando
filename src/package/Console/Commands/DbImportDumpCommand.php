0332f4afad440b70b501bd1783e2517424e6b995
Vladimir Ković <vlada.kovic@gmail.com>
2019-06-28 18:13:10 +0200

    db:import-dump improvements and initial formatter created

--- src/package/Console/Commands/DbImportDumpCommand.php
+++ src/package/Console/Commands/DbImportDumpCommand.php
@@ -3,13 +3,13 @@
 namespace Vkovic\LaravelCommandos\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Console\ConfirmableTrait;
+use Vkovic\LaravelCommandos\Console\FormatOutput;
 use Vkovic\LaravelCommandos\Handlers\Console\WithConsoleHandler;
 use Vkovic\LaravelCommandos\Handlers\Database\WithDbHandler;
 
 class DbImportDumpCommand extends Command
 {
-    use ConfirmableTrait, WithDbHandler, WithConsoleHandler;
+    use WithDbHandler, WithConsoleHandler, FormatOutput;
 
     /**
      * The name and signature of the console command.
@@ -18,8 +18,7 @@ class DbImportDumpCommand extends Command
      */
     protected $signature = 'db:import-dump 
                                 {database? : Which database to use as import destination. Db from env will be used if none passed}
-                                {dump? : Which dump file to use as import source. TODO: Which one will be used if none passed}
-                                {--force : Force operation to run when in production.}
+                                {--dir= : Directory to scan for sql dumps. If omitted default filesystem dir will be used}
                            ';
 
     /**
@@ -29,29 +28,41 @@ class DbImportDumpCommand extends Command
      */
     protected $description = 'Import dump to database (env default or passed one)';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         // TODO
         // Implement arguments that user can pass to customize dump process.
         // Also add easy gzip fn
 
-        // Check if requirenments are met
+        // Check if requirements are met
         if (!$this->consoleHandler()->commandExists('mysql')) {
             $this->warn('`mysql` program required');
+
             return $this->info('e.g. on Ubuntu you can install `mysql` by running `apt install mysql-client`');
         }
 
-        // Prepara db dump file
-        $dump = $this->argument('dump') ?: storage_path('dump.sql');
+        //
+        // Choose dump to load
+        //
 
-        if (!file_exists($dump)) {
-            throw new \Exception('Dump file for import not exist');
+        $dir = $this->option('dir')
+            ?: config('filesystems.disks.' . config('filesystems.default') . '.root');
+
+        $dumps = [];
+        foreach (glob($dir . DIRECTORY_SEPARATOR . '*.sql') as $file) {
+            $dumps[] = str_replace($dir . DIRECTORY_SEPARATOR, '', $file);
         }
+
+        if (empty($dumps)) {
+            $this->warn('There`s no .sql dump files available at ' . $dir);
+
+            return $this->warn('You can specify custom dir with `--dir` option');
+        }
+
+        // Show choices ordered by dump file name
+        asort($dumps);
+        $this->skipLine()->line('Lookup dir: ' . $dir);
+        $dump = $this->choice('Choose dump to be imported:', array_reverse($dumps), 0);
 
         // Get database name either from passed argument (if any)
         // or from default database configuration
@@ -113,6 +124,7 @@ class DbImportDumpCommand extends Command
         $database = env('DB_DATABASE');
         $host = env('DB_HOST');
 
+        $dump = $dir . DIRECTORY_SEPARATOR . $dump;
         $command = "mysql -h $host -u$user -p$password $database < $dump";
 
         $this->consoleHandler()->executeCommand($command);
---
+++ src/package/Console/FormatOutput.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Vkovic\LaravelCommandos\Console;
+
+trait FormatOutput
+{
+    /**
+     * Force all output to be one space from beginning of the screen
+     * for nicer output
+     *
+     * @param      $string
+     * @param null $style
+     * @param null $verbosity
+     */
+    public function line($string, $style = null, $verbosity = null)
+    {
+        parent::line(' ' . $string . ' ', $style, $verbosity);
+    }
+
+    /**
+     * Make one empty line
+     *
+     * @return $this
+     */
+    public function skipLine()
+    {
+        parent::line('');
+
+        return $this;
+    }
+
+    /**
+     * Skip one line at the end of each finished command
+     */
+    public function __destruct()
+    {
+        $this->skipLine();
+    }
+}
